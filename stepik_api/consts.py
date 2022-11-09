4654ba21e6092a324df73ac4dd98e4fd6ecf766c
OldKrab <4shushakov@gmail.com>
2022-11-09 12:11:09 +0300

    add type consts

--- stepik_api/consts.py
+++ stepik_api/consts.py
@@ -12,3 +12,19 @@ COURSES_PK = COURSES + "{}"
 SECTIONS = STEPIK_API_URL + "/sections/"
 
 UNITS = STEPIK_API_URL + "/units/"
+
+TYPE_CHOICE = "choice"
+TYPE_SORTING = "sorting"
+TYPE_MATCHING = "matching"
+TYPE_TABLE = "table"
+TYPE_FILL_BLANKS = "fill-blanks"
+TYPE_NUMBER = "number"
+TYPE_MATH = "math"
+TYPE_RANDOM_TASKS = "random-tasks"
+TYPE_STRING = "string"
+TYPE_FREE_ANSWER = "free-answer"
+TYPE_CODE = "code"
+TYPE_SQL = "sql"
+TYPE_TEXT = "text"
+
+SUPPORTED_TYPES = {TYPE_CHOICE, TYPE_TEXT, TYPE_NUMBER}
