fa930ea25393529102662fd81a26c5df8c0cce32
Olga Bachishe <bachisheo@gmail.com>
2022-11-09 18:52:27 +0300

    realized simple converter

--- converter.py
+++ converter.py
@@ -2,35 +2,38 @@ from stepik_api.quiz_jsons import Quiz
 from stepik_api import consts
 from anki_connect_module.anki_connect_sender import AnkiConnectorSender
 
+HARD_DECK = "duck"
+
+def convert_simple(quiz):
+    question = quiz.step["block"]["text"] 
+    answer = quiz.answer[quiz.type]
+    return question, answer
+
+def choice_formatter(quiz: Quiz):
+    if quiz.attempt['dataset'][consts.MULT_CHOICE_FIELD]:
+        quest = "Выберите несколько вариантов из списка"
+    else: 
+        quest = "Выберите один вариант из списка"
+    #doto this
+    
+def convert_choice(quiz: Quiz):
+    options, answer = choice_formatter(quiz)
+    question = quiz.step["block"]["text"] + '\n' + options 
+    answer = quiz.answer[quiz.type]
+    return question, answer
+
 converters = {
-    consts.TYPE_NUMBER: convert_number,
-    consts.TYPE_STRING: convert_string,
+    consts.TYPE_NUMBER: convert_simple,
+    consts.TYPE_STRING: convert_simple,
     consts.TYPE_CHOICE: convert_choice,
 }
 
-HARD_DECK = "duck"
-def convert_number(quiz):
-    question = quiz.step["block"]["text"] 
-    answer = quiz.answer["number"]
+# convert step into anki with types
+# return map with structure as a note
+def convert(quiz):
+    question, answer = converters[quiz.type]
     return {
         "deckName": HARD_DECK,
         "modelName": "Basic",
         "fields": {"Front": question, "Back": answer},
     }
-
-def convert_string(quiz):
-    question = quiz.step["block"]["text"] 
-    answer = quiz.answer["string"]
-    return {
-        "deckName": HARD_DECK,
-        "modelName": "Basic",
-        "fields": {"Front": question, "Back": answer},
-    }
-
-# convert all steps into anki with types
-# return map with structure as a notes
-def convert_all(quizzes: Quiz):
-    notes = {}
-    for q in quizzes:
-        notes.pop(converters[q.type](q))
-    return
