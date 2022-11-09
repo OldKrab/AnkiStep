a52435067fdc6d56a0f5a2c512f40ff43955930b
Nikolai-SE <nikolai.stoiko@yandex.ru>
2022-11-09 16:28:06 +0300

    Add prototype of api

---
+++ api/api.py
@@ -0,0 +1,22 @@
+
+from anki_connect_module.anki_connect_sender import AnkiConnectorSender
+from stepik_api.authorisation import OAuthStepik
+from stepik_api.data_loader import DataLoader
+
+class AnkiStepAPI:
+
+    anki_connect_sender = AnkiConnectorSender()
+    stepic_oauth = OAuthStepik()
+    loader = DataLoader()
+    stepik_quizes = None
+    anki_notes = None
+
+    def authorize(self, client_id: str, client_secret: str, username: str = None, password: str = None):
+        self.stepic_oauth.set_credentials(client_id, client_secret, username)
+        self.stepic_oauth.auth_user_password(password)
+
+    def load_stepik_course(self, quiz_id):
+        self.stepik_quizes = self.loader.load_quizes(quiz_id)
+
+    def save_quizes_anki(self):
+        pass
